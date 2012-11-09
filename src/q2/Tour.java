d7ffda95c5b2d4a12e9a6950ee37e5aac8f9b988
Salma Hmeida <salma.hmeida@u-psud.fr>
2012-11-09 10:31:39 +0100

    version1

--- src/q2/Tour.java
+++ src/q2/Tour.java
@@ -1,22 +1,25 @@
 package q2;
 public class Tour extends Pile{
-	  public Tour(int capa) {
-		  super(capa);
-	    }
+	public Tour(int capa) {
+		super(capa);
+	}
 
-	    public void remplir(int nb) throws ErreurPile {
-		for(int i = nb; i > 0; i--) 
-		    this.empiler(new Disque(i));
-	    }
+	public void remplir(int nb) throws ErreurPile {
+		for(int i = nb; i > 0; i--){
+			Object d=(Empilable)new Object();
+			((Empilable)d).setVal(i);
+			this.empiler(d);
+		}
+	}
 
-	    public void empiler(Object v) throws ErreurPile {
-	    try{		
-	    	  if(this.estVide() || (((Disque)this.sommet()).compareTo((Disque)v)>0)) super.empiler((Disque)v);
-	          else throw new ErreurTour("le sommet est plus petit que le disque a empiler!!");
-	    }catch(ClassCastException e){
-	    	System.out.println("Ops! l'objet a empiler n'est pas un disque, la tour rejete");//le cas ou la tour est vide et v#disque
-	    }
-	      
-	   }
+	public void empiler(Empilable v) throws ErreurPile {
+		try{		
+			if(this.estVide() || (((Empilable)this.sommet()).compareTo((Empilable)v)>0)) super.empiler((Disque)v);
+			else throw new ErreurTour("le sommet est plus petit que l'object a empiler!!");
+		}catch(ClassCastException e){
+			System.out.println("Ops! l'objet a empiler n'est pas un empilable, la tour rejete");//le cas ou la tour est vide et v#disque
+		}
+
+	}
 
 }
